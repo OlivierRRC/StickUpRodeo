6b1a9a16052853c955067f51476970ed3d590013
Olivier Proulx <oproulx@academic.rrc.ca>
2025-02-23 16:02:09 -0600

    added win condition

--- Assets/Scripts/EnemyManager.cs
+++ Assets/Scripts/EnemyManager.cs
@@ -1,16 +1,31 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class EnemyManager : MonoBehaviour
 {
+    GameObject[] enemies;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        enemies = GameObject.FindGameObjectsWithTag("Enemy");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        for (int i = 0; i < enemies.Length; i++)
+        {
+            if (enemies[i] == null)
+            {
+                enemies = GameObject.FindGameObjectsWithTag("Enemy");
+            }
+        }
+
+        if (enemies.Length == 0)
+        {
+            SceneManager.LoadScene("Win");
+
+        }
     }
 }
