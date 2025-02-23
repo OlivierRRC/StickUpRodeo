fa1717a2b192f1ea08ea7908deb46bf76222d6af
szmonjbob <szmonjbob@gmail.com>
2025-02-23 13:05:23 -0600

    Added Sound Effects and Colour Transitions to the main menu

--- Assets/Scripts/MainMenuManager.cs
+++ Assets/Scripts/MainMenuManager.cs
@@ -1,12 +1,20 @@
 using UnityEngine;
-
+[RequireComponent(typeof(AudioSource))]
 public class MainMenuManager : MonoBehaviour
 {
+    AudioSource audioClip; 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         Cursor.visible = true;
         Cursor.lockState = CursorLockMode.None;
+        audioClip = GetComponent<AudioSource>();
+    }
+
+    public void PlaySound(AudioClip clip)
+    {
+        audioClip.clip = clip;
+        audioClip.Play();
     }
 
     public void StartGame(string scene)
